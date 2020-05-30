1128f2bc25291fe7c78633036c6e5bf8a6feb115
Abhay Pratap Singh <abhaypratap3536@gmail.com>
2020-05-30 23:33:05 +0100

    Updates

---
+++ precache-manifest.24ccb31b155ad740d093514d0b9a4f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cc13eb35f3c50543e18",
+    "url": "/music-react/static/css/main.89cd10bf.chunk.css"
+  },
+  {
+    "revision": "7cc13eb35f3c50543e18",
+    "url": "/music-react/static/js/main.7cc13eb3.chunk.js"
+  },
+  {
+    "revision": "5d7850cd7effeb1059cf",
+    "url": "/music-react/static/js/1.5d7850cd.chunk.js"
+  },
+  {
+    "revision": "346e1c3c32f74a9a5a3c",
+    "url": "/music-react/static/js/runtime~main.346e1c3c.js"
+  },
+  {
+    "revision": "8458127fcdf67f01de800f5b000e593e",
+    "url": "/music-react/index.html"
+  }
+];
